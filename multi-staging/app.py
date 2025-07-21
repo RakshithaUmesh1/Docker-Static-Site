4e390bc7c5a0c367a3698d22faeb2e7a4b867040
Rakshitha <Deepumadiwalzz720@gmail.com>
2025-07-21 12:23:25 +0000

    multi-staging foder added

---
+++ multi-staging/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello():
+    return "hello guys this is our first flask app"
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5005)
